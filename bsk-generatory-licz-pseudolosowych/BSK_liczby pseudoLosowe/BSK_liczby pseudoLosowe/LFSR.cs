03d275eb15fc45d2e81ddd16949c5c5823d0a1a2
matiprogrammer <37975088+matiprogrammer@users.noreply.github.com>
2020-09-16 18:14:37 +0200

    Add files via upload
    project added

---
+++ bsk-generatory-licz-pseudolosowych/BSK_liczby pseudoLosowe/BSK_liczby pseudoLosowe/LFSR.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSK_liczby_pseudoLosowe
+{
+    public class LFSR
+    {
+        int[] seed;
+        int[] polynomial;
+
+
+        public LFSR(string polynomialText, string seedText)
+        {
+            seed = ConvertSringToIntTable(seedText);
+            polynomial = ConvertSringToIntTable(polynomialText);
+
+
+        }
+
+        public string GenerateNext()
+        {
+            int[] nextValue = null;
+            nextValue = seed.ToArray();
+
+
+            for (int i = polynomial.Length - 1; i > 0; i--)
+            {
+                nextValue[i] = nextValue[i - 1];
+            }
+            var a = nextValue;
+            int temp = 0;
+
+            for (int i = 0; i < polynomial.Length; i++)
+            {
+                if (polynomial[i] == 1 && seed[i] == 1)
+                    temp++;
+            }
+            temp = temp % 2;
+
+            if (temp == 0)
+                temp = 0;
+            else
+                temp = 1;
+            nextValue[0] = temp;
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < polynomial.Length; i++)
+            {
+                result.Append(nextValue[i]);
+
+            }
+            seed = nextValue;
+            return result.ToString();
+
+        }
+
+        private int[] ConvertSringToIntTable(string inputString)
+        {
+            int[] temp = new int[inputString.Length];
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                temp[i] = int.Parse(inputString[i].ToString());
+            }
+            return temp;
+        }
+
+    }
+}
---
+++ bsk-generatory-licz-pseudolosowych/BSK_liczby pseudoLosowe/BSK_liczby pseudoLosowe/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BSK_liczby_pseudoLosowe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenZad1(object sender, RoutedEventArgs e)
+        {
+            Zad1Window zad1 = new Zad1Window();
+            zad1.Show();
+
+        }
+
+        private void OpenZad3(object sender, RoutedEventArgs e)
+        {
+            Zad3Window zad3 = new Zad3Window();
+            zad3.Show();
+        }
+
+        private void OpenZa2(object sender, RoutedEventArgs e)
+        {
+            Zad2Window zad2 = new Zad2Window();
+            zad2.Show();
+        }
+    }
+}
